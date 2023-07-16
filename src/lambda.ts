01478e8e72ac8f41c6d5189013f6034fd7358914
Uday Singh <udaysingh236@gmail.com>
2023-07-16 18:06:16 +0530

    more granular logging

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,55 +2,59 @@ import 'dotenv/config';
 import app from './app';
 import { logger } from './utils/logger';
 import mongoose from 'mongoose';
-import serverless from 'serverless-http';
+import serverless, { Handler } from 'serverless-http';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const mongoUri: string = process.env.MONGO_URI!;
 
 const handler = serverless(app);
-module.exports.handler = async (event: object, context: object) => {
+module.exports.handler = async (event: Handler, context: Handler) => {
+    // Make sure to add this so you can re-use `conn` between function calls.
+    // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore:
+    // context.callbackWaitsForEmptyEventLoop = false;
     await mongoose.connect(mongoUri);
     logger.info(`Connected to MongoDB from Lambda handler`);
     const result = await handler(event, context);
-    // and here
     return result;
 };
 
 // Listen to all the error events that may be raised when the app is live
 mongoose.connection.on('error', () => {
-    logger.fatal(`Error connecting to MongoDB..!!!`);
+    logger.fatal(`From Lambda - Error connecting to MongoDB..!!!`);
     process.exit(1);
 });
 
 mongoose.connection.on('disconnected', () => {
-    logger.fatal(`Disconnected from MongoDB..!!!`);
+    logger.fatal(`From Lambda - Disconnected from MongoDB..!!!`);
     process.exit(1);
 });
 
 mongoose.connection.on('connecting', () => {
-    logger.info(`Trying to connect to MongoDB`);
+    logger.info(`From Lambda - Trying to connect to MongoDB`);
 });
 
 mongoose.connection.on('connected', () => {
-    logger.info(`Connected to MongoDB.!!!`);
+    logger.info(`From Lambda - Connected to MongoDB.!!!`);
 });
 
 process.on('SIGINT', function () {
-    logger.info('Caught SIGINT signal');
+    logger.info('From Lambda - Caught SIGINT signal');
     // Close mongoDB connection then exit
     mongoose.connection.close();
     process.exit(0);
 });
 
 process.on('SIGQUIT', function () {
-    logger.info('Caught SIGQUIT signal');
+    logger.info('From Lambda - Caught SIGQUIT signal');
     // Close mongoDB connection then exit
     mongoose.connection.close();
     process.exit(0);
 });
 
 process.on('SIGTERM', function () {
-    logger.info('Caught SIGTERM signal');
+    logger.info('From Lambda - Caught SIGTERM signal');
     // Close mongoDB connection then exit
     mongoose.connection.close();
     process.exit(1);
--- src/server.ts
+++ src/server.ts
@@ -23,39 +23,39 @@ startServer(mongoUri);
 
 // Listen to all the error events that may be raised when the app is live
 mongoose.connection.on('error', () => {
-    logger.fatal(`Error connecting to MongoDB..!!!`);
+    logger.fatal(`From Local - Error connecting to MongoDB..!!!`);
     process.exit(1);
 });
 
 mongoose.connection.on('disconnected', () => {
-    logger.fatal(`Disconnected from MongoDB..!!!`);
+    logger.fatal(`From Local - Disconnected from MongoDB..!!!`);
     process.exit(1);
 });
 
 mongoose.connection.on('connecting', () => {
-    logger.info(`Trying to connect to MongoDB`);
+    logger.info(`From Local - Trying to connect to MongoDB`);
 });
 
 mongoose.connection.on('connected', () => {
-    logger.info(`Connected to MongoDB.!!!`);
+    logger.info(`From Local - Connected to MongoDB.!!!`);
 });
 
 process.on('SIGINT', function () {
-    logger.info('Caught SIGINT signal');
+    logger.info('From Local - Caught SIGINT signal');
     // Close mongoDB connection then exit
     mongoose.connection.close();
     process.exit(0);
 });
 
 process.on('SIGQUIT', function () {
-    logger.info('Caught SIGQUIT signal');
+    logger.info('From Local - Caught SIGQUIT signal');
     // Close mongoDB connection then exit
     mongoose.connection.close();
     process.exit(0);
 });
 
 process.on('SIGTERM', function () {
-    logger.info('Caught SIGTERM signal');
+    logger.info('From Local - Caught SIGTERM signal');
     // Close mongoDB connection then exit
     mongoose.connection.close();
     process.exit(1);
